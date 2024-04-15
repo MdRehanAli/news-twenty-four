6a5e7f5ca8a9321842d26da849aa307e249d752d
MdRehanAli <rehanali21d@gmail.com>
2024-04-15 20:53:36 +0600

    Added all setup and hero section

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,28 @@
+import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+import Navbar from "../Shared/navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h2 className='text-3xl'>This is home.</h2>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div>
+                <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2">
+                    <h2 className="text-2xl">News</h2>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>Right side Nav</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
