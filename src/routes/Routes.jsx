715640ad1948c739e2c8b7716fcbd888b9aca3a3
MdRehanAli <rehanali21d@gmail.com>
2024-04-20 14:39:10 +0600

    firebase deploy

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
             },
             {
                 path: '/login',
-                element: <Login></Login>
+                element: <Login></Login>    
             },
             {
                 path: '/register',
